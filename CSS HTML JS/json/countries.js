84e3511aeb777e415180f2380c7888d3787e1eba
AnastasiyaSam <71564025+AnastasiyaSam@users.noreply.github.com>
2020-10-18 17:29:09 +0200

    Cleaner version with fake json

---
+++ CSS HTML JS/json/countries.js
@@ -0,0 +1,22 @@
+var countries_data = {
+    "Germany Berlin" : {
+        "url" : "www.google.com",
+        "case_number" : "10",
+        "comments" : "wear masks"
+    },
+    "Sweden" : {
+        "url" : "ww.fitta.se",
+        "case_number" : "20",
+        "comments" : "don't wear masks"
+    },
+    "Portugal" : {
+        "url" : "www.google.com",
+        "case_number" : "20",
+        "comments" : "wear masks"
+    },
+    "Belgium" : {
+        "url" : "www.google.com",
+        "case_number" : "20",
+        "comments" : "wear masks"
+    }
+};
