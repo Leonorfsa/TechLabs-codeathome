055c44ae05ef563b274f99133498c8a689d295cf
AnastasiyaSam <71564025+AnastasiyaSam@users.noreply.github.com>
2020-10-27 18:14:46 +0100

    Alarm correction

--- js/dropdowntable.js
+++ js/dropdowntable.js
@@ -24,38 +24,39 @@ function myFunction() {
     })
 
     /* alarm when from or to is missing*/
-    if (from == "Choose starting point" || to == "Choose your destination") {
+    if (from == "Choose starting point" || to == "Choose your destination" || from == to) {
         alert("You must choose a starting point and your destination.");
     }
+    else {
+        var inlink1 = null
+        var inlink2 = null
+        if (col1[0].https == "y") {
+            inlink1 = '<a href=https://' + col1[0].Links + ' target="_blank">More Information</a> ' 
+                    + '</td><td>' 
+        } else {
+            inlink1 = '<a href=http://' + col1[0].Links + ' target="_blank">More Information</a> ' 
+                    + '</td><td>' 
+                    }
 
-    var inlink1 = null
-    var inlink2 = null
-    if (col1[0].https == "y") {
-        inlink1 = '<a href=https://' + col1[0].Links + ' target="_blank">More Information</a> ' 
-                + '</td><td>' 
-    } else {
-        inlink1 = '<a href=http://' + col1[0].Links + ' target="_blank">More Information</a> ' 
-                + '</td><td>' 
-                }
+        if (col2[0].https == "y") {
+            inlink2 = '<a href=https://' + col2[0].Links + ' target="_blank">More Information</a> ' 
+        } else {
+            inlink2 = '<a href=http://' + col2[0].Links + ' target="_blank">More Information</a> ' 
+            }
 
-    if (col2[0].https == "y") {
-        inlink2 = '<a href=https://' + col2[0].Links + ' target="_blank">More Information</a> ' 
-    } else {
-        inlink2 = '<a href=http://' + col2[0].Links + ' target="_blank">More Information</a> ' 
-        }
-
-    items_table.innerHTML = null;
-    items_table = document.getElementById('items_table');
-    items_table.innerHTML+= '<tr><th>' + " Categories" + '</th><th>' + "From: " +col1[0].town  + 
-        '</th><th>' +"To: "+ col2[0].town +'</th></tr>'+
-        '<tr><td>' + "Entry Regulations" + '</td><td>' 
-            + col1[0].Entry + '</td><td>' + col2[0].Entry + '</td></tr>' +
-        '<tr><td>' + "Hygiene Regulations" + '</td><td>' 
-            + col1[0].Hygiene + '</td><td>' + col2[0].Hygiene + '</td></tr>' +
-        '<tr><td>' + "Activity Regulations" + '</td><td>' 
-            + col1[0].Activity + '</td><td>' + col2[0].Activity + '</td></tr>' +
-        '<tr><td>' + "Further Links" + '</td><td>' 
-        + inlink1 + inlink2 
-        + '</td></tr>' + '<tr><td>' + "Last Updated:" + '</td><td>' 
-            + col1[0].Dates + '</td><td>' + col2[0].Dates + '</td></tr>';
+        items_table.innerHTML = null;
+        items_table = document.getElementById('items_table');
+        items_table.innerHTML+= '<tr><th>' + " Categories" + '</th><th>' + "From: " +col1[0].town  + 
+            '</th><th>' +"To: "+ col2[0].town +'</th></tr>'+
+            '<tr><td>' + "Entry Regulations" + '</td><td>' 
+                + col1[0].Entry + '</td><td>' + col2[0].Entry + '</td></tr>' +
+            '<tr><td>' + "Hygiene Regulations" + '</td><td>' 
+                + col1[0].Hygiene + '</td><td>' + col2[0].Hygiene + '</td></tr>' +
+            '<tr><td>' + "Activity Regulations" + '</td><td>' 
+                + col1[0].Activity + '</td><td>' + col2[0].Activity + '</td></tr>' +
+            '<tr><td>' + "Further Links" + '</td><td>' 
+            + inlink1 + inlink2 
+            + '</td></tr>' + '<tr><td>' + "Last Updated:" + '</td><td>' 
+                + col1[0].Dates + '</td><td>' + col2[0].Dates + '</td></tr>';
+    }
 }
