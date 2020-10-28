45efe30209f0c29ff0d8e42d6e8a1ecb536dd4f8
RikeS <45359179+Akimasu@users.noreply.github.com>
2020-10-28 15:20:21 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -5,6 +5,9 @@ mydata = [
         "Entry": "Restricted Entry if you are visiting from a high risk area see link below from the RKI or if you are coming from outside of europe.",
         "Hygiene": "Masks are necessary during shopping and travelling per public transports.",
         "Activity": "Many activities are allowed with sometimes limited access.",
+        "Departure": "",
+        "HygieneDep": "Masks are necessary during shopping and travelling per public transports.",
+        "Other": "",
         "Links": "experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/page_0/",
         "Dates": "20.Oct, 2020",
         "https":"y"
@@ -15,6 +18,9 @@ mydata = [
         "Entry": "There is an entry ban for non-essential travel to Sweden from countries outside the EU. The entry ban does not apply to citizens of EU/EEA countries, UK, Switzerland, Andorra, Monaco, San Marino and the Vatican, or their families, or for travels to Sweden from the EU/EEA area. Furthermore, the ban does not apply if you or a close relative already live here as a permanent resident or hold a residence permit in Sweden or another EU country, or if you have a national visa to Sweden. You do not need the coronavirus test",
         "Hygiene": "There is no general requirement to wear a face mask in normal social situations, in public places, in public transport etc.",
         "Activity": "Public gatherings and events may have a maximum of 50 participants. The Swedish Government has decided that restaurants and cafes shall be exempt from the 50-person limit. The change applies from 8 October 2020.",
+        "Departure": "",
+        "HygieneDep": "Masks are necessary during shopping and travelling per public transports.",
+        "Other": "",
         "Links": "www.krisinformation.se/en/hazards-and-risks/disasters-and-incidents/2020/official-information-on-the-new-coronavirus/visiting-sweden-during-the-covid-19-pandemic",
         "Dates": "25.Oct, 2020",
         "https":"n"
@@ -25,6 +31,9 @@ mydata = [
         "Entry": "You must present, at the time of arrival, a proof of laboratory test for screening for infection by SARS-CoV-2 with a negative result.", 
         "Hygiene": "Masks are necessary in closed public spaces and travelling per public transports. Required 2 meters distancing at all times. The Clean&Safe seal allows the identification of establishments and services that have implemented reinforced hygiene and safety measures.",
         "Activity": "Many activities are allowed with sometimes limited access.", 
+        "Departure": "",
+        "HygieneDep": "Masks are necessary during shopping and travelling per public transports.",
+        "Other": "",
         "Links": "visitmadeira.pt/en-gb/homepage?AreaId=16",
         "Dates": "24.Oct, 2020",
         "https":"n"
@@ -35,6 +44,9 @@ mydata = [
         "Entry": "If you are visiting from a high risk country you must present, at the time of departure from these countries, a proof of laboratory test for screening for infection by SARS-CoV-2 with a negative result.", 
         "Hygiene": "Masks are necessary in closed public spaces and travelling per public transports. Required 2 meters distancing at all times. The Clean&Safe seal allows the identification of establishments and services that have implemented reinforced hygiene and safety measures.",
         "Activity": "Forbidden activities with more than 5 people. Public spaces must close before 1am and alcoholic beverages are forbidden after 8pm. Bars and discos must remain closed.",  
+        "Departure": "",
+        "HygieneDep": "Masks are necessary during shopping and travelling per public transports.",
+        "Other": "",
         "Links": "covid19.min-saude.pt/#",
         "Dates": "24.Oct, 2020",
         "https":"y"
@@ -45,6 +57,9 @@ mydata = [
         "Entry": "You must present, at the time of arrival, a proof of laboratory test for screening for infection by SARS-CoV-2 with a negative result.", 
         "Hygiene": "Masks are obligatory at all times. The Clean&Safe seal allows the identification of establishments and services that have implemented reinforced hygiene and safety measures.There is no quarantine requirement for travellers to Sweden.",
         "Activity": "Many activities are allowed with sometimes limited access.",
+        "Departure": "",
+        "HygieneDep": "Masks are necessary during shopping and travelling per public transports.",
+        "Other": "",
         "Links":"visitmadeira.pt/en-gb/homepage?AreaId=16",
         "Dates": "20.Oct, 2020",
         "https":"n"
